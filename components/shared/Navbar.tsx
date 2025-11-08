2668bc7e29c043e048957dd0c3edb550db0c4f26
sajib37 <sajibhasan9811@gmail.com>
2025-11-08 12:10:31 +0600

    project initialize !

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#blog">Blog</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
